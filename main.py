992067fb12c73e7b785eba1216d81625db105542
steve28 <steve28@github.com>
2018-03-11 15:13:34 -0700

    updated MQTT client ID and report temp every 60s

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ pin_led.value(1)
 
 
 # define some globals
-prev_temp = 0
-prev_humid = 0
 prev_left_contact = -1
 prev_right_contact = -1
 last_check = 0
@@ -34,13 +32,19 @@ TOPIC_TEMPERATURE = b"garage/temperature"
 TOPIC_HUMIDITY = b"garage/humidity"
 
 def do_connect():
+    # Get wlan
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
+    # Disable the AP WebRepl
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
     if not wlan.isconnected():
         print('connecting to network...')
         wlan.connect('Magnum', 'supersecretpassword')
         while not wlan.isconnected():
             pass
+    else:
+        print("Already connected...")
     print('network config:', wlan.ifconfig())
 
 def push(relay):
@@ -49,33 +53,36 @@ def push(relay):
     relay.on()
 
 def mqtt_callback(topic, payload):
+    global prev_left_contact, prev_right_contact
+
     print("Topic:",topic,"Payload:", payload)
-    print("prev_left_contact:", prev_left_contact)
-    print("prev_right_contact:", prev_right_contact)
+
     if topic == TOPIC_LEFT_DOOR_CMD:
         if (payload == b'open') and (prev_left_contact == 0):
             push(pin_left_relay)
-
         if (payload == b'close') and (prev_left_contact == 1):
             push(pin_left_relay)
 
     if topic == TOPIC_RIGHT_DOOR_CMD:
         if (payload == b'open') and (prev_right_contact == 0):
             push(pin_right_relay)
-
         if (payload == b'close') and (prev_right_contact == 1):
             push(pin_right_relay)
 
+    if payload == b'refresh':
+        print("Forcing refresh of door status")
+        prev_left_contact = -1
+        prev_right_contact = -1
 
 def main():
     # globals
-    global prev_left_contact, prev_right_contact, prev_temp, prev_humid, last_check
+    global prev_left_contact, prev_right_contact, last_check
 
     # connect to the WLAN
     do_connect()
 
     # connect to the MQTT server
-    c = MQTTClient("my_client","192.168.1.8")
+    c = MQTTClient("garage","192.168.1.8")
     c.set_callback(mqtt_callback)
     c.connect()
     c.subscribe(TOPIC_LEFT_DOOR_CMD)
@@ -89,7 +96,7 @@ def main():
 
         # Update them if necessary
         if (prev_left_contact != left_contact):
-            print("Update left contact")
+            print("Left door status changed to:", left_contact)
             prev_left_contact = left_contact
             if left_contact == 0:
                 c.publish(TOPIC_LEFT_DOOR_STATUS,b"closed")
@@ -97,7 +104,7 @@ def main():
                 c.publish(TOPIC_LEFT_DOOR_STATUS,b"open")
 
         if (prev_right_contact != right_contact):
-            print("Update right contact")
+            print("Right door status changed to:", right_contact)
             prev_right_contact = right_contact
             if right_contact == 0:
                 c.publish(TOPIC_RIGHT_DOOR_STATUS,b"closed")
@@ -108,25 +115,22 @@ def main():
         # and it has been longer than 5s
         now = ticks_ms()
         if now-last_check > 60000:
-            print("Checking DHT22")
+            print("Checking DHT22...",end='')
             last_check = now
             try:
                 th_sens.measure()
                 temp =  th_sens.temperature()
                 humid = th_sens.humidity()
-
-                if prev_temp != temp:
-                    prev_temp = temp
-                    temp_bytestring = "{:.1f}".format(temp*1.8+32.0)
-                    c.publish(TOPIC_TEMPERATURE,
-                              ustruct.pack('{}s'.format(len(temp_bytestring)),temp_bytestring))
-
-                if prev_humid != humid:
-                    prev_humid = humid
-                    c.publish(TOPIC_HUMIDITY,
-                              ustruct.pack('{}s'.format(len(str(humid))),str(humid)))
+                print("OK")
+                temp_bytestring = "{:.1f}".format(temp*1.8+32.0)
+                c.publish(TOPIC_TEMPERATURE,
+                          ustruct.pack('{}s'.format(len(temp_bytestring)),temp_bytestring))
+                prev_humid = humid
+                c.publish(TOPIC_HUMIDITY,
+                          ustruct.pack('{}s'.format(len(str(humid))),str(humid)))
             except:
-                print("Error reading DHT22")
+                print("Error")
+
         # Check for incoming MQTT messages
         c.check_msg()
 
